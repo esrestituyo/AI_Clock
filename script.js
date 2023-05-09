77c7828b2332868622b0502bf5dd3abaa7755d9c
esrestituyo <124016214+esrestituyo@users.noreply.github.com>
2023-05-09 18:22:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function currentTime() {
         hh = 12;
     }
     if (hh > 12) {
-        hh = hh;
+        hh = hh - 12;
         session = "PM";
     }
     hh = (hh < 10) ? "0" + hh : hh;
@@ -17,13 +17,13 @@ function currentTime() {
     let time = hh + ":" + mm + ":" + ss + " " + session;
     
     // Set greeting message based on current hour
-    console.log(hh);
-    console.log(session)
+    // console.log(hh);
+    // console.log(session)
 
     let greeting;
-    if ((hh >= 6 && hh < 12) && (session = "AM")) {
+    if (session === "AM") {
         greeting = "Buenos dias, la hora exacta...";
-    } else if ((hh >= 12 && hh < 18) && (session = "PM")) {
+    } else if (session === "PM" && hh<=7) {
         greeting = "Buenas tardes, la hora exacta...";
     } else {
         greeting = "Buenas noches, la hora exacta...";
@@ -36,3 +36,29 @@ function currentTime() {
     }, 1000);
 }
 currentTime();
+
+function getWeather(city, callback) {
+    var url = 'https://api.openweathermap.org/data/2.5/weather?appid=60acd26add5f2b4eeaf7d840d0a8b8b4&units=metric&q=Santo Domingo,DO';
+    var apiKey = "5e017caa041adc8c7e499bdfe5a10f53";
+    $.ajax({
+        dataType: "jsonp",
+        url: url,
+        jsonCallback: 'jsonp',
+        data: { q: city, appid: apiKey },
+        cache: false,
+        success: function(data) {
+            callback(data.main.temp);
+            console.log(data);
+        }
+    });
+}
+
+getWeather('Santo Domingo, DO', function(temp) {
+    // Update greeting with temperature information
+    let greetingtemp = "La temperatura... "
+    let temperature = Math.ceil(temp) + ' grados centigrados';
+    document.getElementById("greetingtemp").innerText = greetingtemp;
+    document.getElementById("temp").innerText = temperature;
+
+console.log(getWeather);
+});
